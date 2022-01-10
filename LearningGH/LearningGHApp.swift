098cce57365515edf892a61acc0c2eae5d95ab5c
Jennifer Bush <jennifer.ryan9@gmail.com>
2022-01-09 18:32:11 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jennifer Bush on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
